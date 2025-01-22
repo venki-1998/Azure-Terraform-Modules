5fd5ac668da05e1e763c1fc5ddad89d0b80df94f
Venkatesh H Pattepur <79396906+venki-1998@users.noreply.github.com>
2025-01-23 00:02:08 +0530

    Create variables.tf

---
+++ terraform-azurerm-eventhub/variables.tf
@@ -0,0 +1,34 @@
+variable "rg_name" {
+  description = "The name of the resource group in which to create event hub. Changing this forces a new resource to be created."
+  type = string
+  default = "IBCI-UAT-TFE-RG01"
+}
+
+variable "namespace_name" {
+  description = "(Required) Specifies the name of the EventHub Namespace. Changing this forces a new resource to be created."
+  type = string
+  default = "Ibcitestnamespace01"
+}
+
+variable "eventhub_name" {
+  description = "(Required) Specifies the name of the EventHub resource. Changing this forces a new resource to be created."
+  type = string
+  default = "myhub"
+}
+
+variable "partition_count" {
+  description = "(Required) Specifies the current number of shards on the Event Hub. Changing this will force-recreate the resource."
+  type = string
+  default = "2"
+}
+
+variable "message_retention" {
+  description = "(Required) Specifies the number of days to retain the events for this Event Hub."
+  type = string
+  default = "1"
+}
+
+#variable "eventhub_tags" {
+  #description = "A mapping of tags to assign to the resource."
+# type = map(any)
+#}
