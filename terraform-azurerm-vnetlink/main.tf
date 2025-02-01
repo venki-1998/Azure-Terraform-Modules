942d545a72bd6a0762f48342ab568f0e0121d6d3
Venkatesh H Pattepur <79396906+venki-1998@users.noreply.github.com>
2025-02-01 23:38:46 +0530

    Create main.tf

---
+++ terraform-azurerm-vnetlink/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
+  name                  = var.vnet_link_name
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = var.dns_zone_name
+  virtual_network_id    = var.vnet_id
+  lifecycle {
+    ignore_changes = [
+        tags
+    ]
+  }
+}
