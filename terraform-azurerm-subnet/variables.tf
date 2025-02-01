b9fb59765eb399fce77167ddb61fc4428f42cc22
Venkatesh H Pattepur <79396906+venki-1998@users.noreply.github.com>
2025-02-01 23:25:33 +0530

    Create variables.tf

---
+++ terraform-azurerm-subnet/variables.tf
@@ -0,0 +1,43 @@
+variable "subnet_name" {
+    type = string
+    description = "(Required) The name of the subnet. Changing this forces a new resource to be created."
+    nullable = false
+}
+
+variable "vnet_name" {
+    type = string
+    description = "(Required) The name of the virtual network to which to attach the subnet. Changing this forces a new resource to be created."
+    nullable = false
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "(Required) The name of the resource group in which to create the subnet. Changing this forces a new resource to be created."
+    validation {
+        condition     = length(var.resource_group_name) > 4
+        error_message = "Lenght of Resource Group Name Should be more than 4."
+    }
+    nullable = false
+}
+
+variable "private_endpoint_network_policies_enabled" {
+    type = bool
+    default = false
+    description = "Enable or Disable network policies for the private endpoint on the subnet. Setting this to true will Enable the policy and setting this to false will Disable the policy. Defaults to False."
+}
+
+variable "subnet_address" {
+    type = list(string)
+    description = "(Required) The address prefixes to use for the subnet."
+    nullable = false
+}
+
+variable "service_endpoints" {
+    default = []
+    type = list(string)
+    description = "The list of Service endpoints to associate with the subnet. Possible values include: Microsoft.AzureActiveDirectory, Microsoft.AzureCosmosDB, Microsoft.ContainerRegistry, Microsoft.EventHub, Microsoft.KeyVault, Microsoft.ServiceBus, Microsoft.Sql, Microsoft.Storage, and Microsoft.Web."
+}
+
+variable "delegation" {
+    description = "name - (Required) The name of service to delegate to. Possible values are Microsoft.ApiManagement/service, Microsoft.AzureCosmosDB/clusters, Microsoft.BareMetal/AzureVMware, Microsoft.BareMetal/CrayServers, Microsoft.Batch/batchAccounts, Microsoft.ContainerInstance/containerGroups, Microsoft.ContainerService/managedClusters, Microsoft.Databricks/workspaces, Microsoft.DBforMySQL/flexibleServers, Microsoft.DBforMySQL/serversv2, Microsoft.DBforPostgreSQL/flexibleServers, Microsoft.DBforPostgreSQL/serversv2, Microsoft.DBforPostgreSQL/singleServers, Microsoft.HardwareSecurityModules/dedicatedHSMs, Microsoft.Kusto/clusters, Microsoft.Logic/integrationServiceEnvironments, Microsoft.LabServices/labplans, Microsoft.MachineLearningServices/workspaces, Microsoft.Netapp/volumes, Microsoft.Network/dnsResolvers, Microsoft.Network/managedResolvers, Microsoft.PowerPlatform/vnetaccesslinks, Microsoft.ServiceFabricMesh/networks, Microsoft.Sql/managedInstances, Microsoft.Sql/servers, Microsoft.StoragePool/diskPools, Microsoft.StreamAnalytics/streamingJobs, Microsoft.Synapse/workspaces, Microsoft.Web/hostingEnvironments, Microsoft.Web/serverFarms, Microsoft.Orbital/orbitalGateways, NGINX.NGINXPLUS/nginxDeployments, PaloAltoNetworks.Cloudngfw/firewalls, and Qumulo.Storage/fileSystems. actions - (Optional) A list of Actions which should be delegated. This list is specific to the service to delegate to. Possible values are Microsoft.Network/networkinterfaces/*, Microsoft.Network/publicIPAddresses/join/action, Microsoft.Network/publicIPAddresses/read, Microsoft.Network/virtualNetworks/read, Microsoft.Network/virtualNetworks/subnets/action, Microsoft.Network/virtualNetworks/subnets/join/action, Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action, and Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action."
+}
